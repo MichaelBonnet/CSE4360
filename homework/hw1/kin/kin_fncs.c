6936f3aa686df07eb9a5031038382f51190c3425
Michael Bonnet <maabonnet@gmail.com>
2021-09-27 23:39:00 -0500

    finished forward kinematics

--- homework/hw1/kin/kin_fncs.c
+++ homework/hw1/kin/kin_fncs.c
@@ -1,238 +1,214 @@
-// #include <stdio.h>
-// #include <math.h>
-
-// #ifndef M_PI
-// #define M_PI 3.1415927
-// #endif
-
-
-// fwd_kin(theta, x)
-// double *theta;
-// double x[3];
-// {
-// }
-
-
-// inv_kin(x, theta)
-// double *x;
-// double theta[6];
-// {
-// }
-
-
-//////////////////////////////////////////////////////////////
-//////////////////////////////////////////////////////////////
-
 #include <stdio.h>
 #include <math.h>
+#include <stdlib.h>
+#include <string.h>
 
 #ifndef M_PI
 #define M_PI 3.1415927
 #endif
 
+const double l0 = 0.25;
+const double l1 = 0.2;
+const double l2 = 0.2;
+const double l3 = 0.15;
 
-double **matrix_multiplication(double mat1[][4],double mat2[][4],double result[][4]){
+const double d1 = -0.04;
+const double d2 = 0.04;
+const double d3 = -0.04;
+const double d4 = -0.04;
 
-int i = 0;
-int j = 0;
-int x = 0;
 
-for(i=0;i<4;i++)
+// allocating memory for a blank matrix
+double** create_matrix()
 {
-	for(j=0;j<4;j++)
+	double** matrix = malloc(4*sizeof(double*));
+
+	for (int i=0; i<4; i++)
 	{
-		result[i][j] = 0.0;
-		for(x=0;x<4;x++)
+		matrix[i] = calloc(4,sizeof(double));
+	}
+
+	return matrix;
+}
+
+// copying a regular array style matrix over to an allocated matrix
+void copy_matrix(double** target, double matrix[4][4])
+{
+	for (int i=0; i<4; i++)
+	{
+		for(int j=0; j<4; j++)
 		{
-			(*(*(result + i) + j)) += (*(*(mat1 + i ) + x)) * (*(*(mat2 + x) + j));
+			target[i][j] = matrix[i][j];
 		}
 	}
 }
 
+// rotation matrix for X
+double** create_Rx(double theta)
+{
+	double Rx[4][4] = 
+	{
+		{1, 0, 0, 0},
+		{0, cos(theta), -1*sin(theta), 0},
+		{0, sin(theta), cos(theta), 0},
+		{0, 0, 0, 1}
+	};
 
+	double** rotmat = create_matrix();
+
+	copy_matrix(rotmat, Rx);
+
+	return rotmat;
 }
 
-
-
-int translation(double x,double y, double z,double D[][4])
+// translation matrix for X
+double** create_Dx(double length)
 {
-	double D1[4][4] = {{1,0,0,x},{0,1,0,y},{0,0,1,z},{0,0,0,1}};
-	int i,j;
-	for(i=0;i<4;i++)
+	double Dx[4][4] = 
 	{
-		for(j=0;j<4;j++)
+		{1, 0, 0, length},
+		{0, 1, 0, 0},
+		{0, 0, 1, 0},
+		{0, 0, 0, 1}
+	};
+
+	double** tranmat = create_matrix();
+
+	copy_matrix(tranmat, Dx);
+
+	return tranmat;
+}
+
+// rotation matrix for Y
+double** create_Ry(double theta)
+{
+	double Ry[4][4] = 
+	{
+		{cos(theta), 0, sin(theta), 0},
+		{0, 1, 0, 0},
+		{-sin(theta), 0, cos(theta), 0},
+		{0, 0, 0, 1}
+	};
+
+	double** rotmat = create_matrix();
+
+	copy_matrix(rotmat, Ry);
+
+	return rotmat;
+}
+
+// translation matrix for y
+double** create_Dy(double length)
+{
+	double Dy[4][4] = 
+	{
+		{1, 0, 0, 0},
+		{0, 1, 0, length},
+		{0, 0, 1, 0},
+		{0, 0, 0, 1}
+	};
+
+	double** tranmat = create_matrix();
+
+	copy_matrix(tranmat, Dy);
+
+	return tranmat;
+}
+
+// rotation matrix for Z
+double** create_Rz(double theta)
+{
+	double Rz[4][4] = 
+	{
+		{cos(theta), -1*sin(theta), 0, 0},
+		{sin(theta), cos(theta), 0, 0},
+		{0, 0, 1, 0},
+		{0, 0, 0, 1}
+	};
+
+	double** rotmat = create_matrix();
+
+	copy_matrix(rotmat, Rz);
+
+	return rotmat;
+}
+
+// translation matrix for Z
+double** create_Dz(double length)
+{
+	double Dz[4][4] = 
+	{
+		{1, 0, 0, 0},
+		{0, 1, 0, 0},
+		{0, 0, 1, length},
+		{0, 0, 0, 1}
+	};
+
+	double** tranmat = create_matrix();
+
+	copy_matrix(tranmat, Dz);
+
+	return tranmat;
+}
+
+// attempting to multiply matrices using memory allocated matrices
+double** multiply_matrix(double** matrix1, double** matrix2)
+{
+	double** result = create_matrix();
+
+	for (int i = 0; i < 4; i++)
+	{
+		for (int j = 0; j < 4; j++)
 		{
-			D[i][j] = D1[i][j];
+			for (int k = 0; k < 4; k++)
+			{
+				result[i][j] += matrix1[i][k] * matrix2[k][j];
+			}
 		}
 	}
-	return 0;
+
+	return result;
 }
 
-int rotation_x(double theta,double R[][4])
+// forward kinematics function
+void fwd_kin(double theta[6], double x[3])
 {
-	double Rx[4][4] = {{1,0,0,0},{0,cos(theta),-1*sin(theta),0},{0,sin(theta),cos(theta),0},{0,0,0,1}};
-	int i,j;
-	for(i=0;i<4;i++)
-	{
-		for(j=0;j<4;j++)
-		{
-			R[i][j] = Rx[i][j];
-		}
-	}
-	return 0;
+	double** T;
+
+	double** Rz_t0 = create_Rz(theta[0]);
+	double** Dz_l0 = create_Dz(l0);
+	double** Ry_t1 = create_Ry(theta[1]);
+	double** Dy_d1 = create_Dy(d1);
+	double** Dx_l1 = create_Dx(l1);
+	double** Ry_t2 = create_Ry(theta[2]);
+	double** Dy_d2 = create_Dy(d2);
+	double** Dx_l2 = create_Dx(l2);
+	double** Ry_t3 = create_Ry(theta[3]);
+	double** Dy_d3 = create_Dy(d3);
+	double** Dz_d4 = create_Dz(d4);
+	double** Dx_l3 = create_Dx(l3);
+
+	T = multiply_matrix(Rz_t0, Dz_l0);
+	T = multiply_matrix(T, Ry_t1);
+	T = multiply_matrix(T, Dy_d1);
+	T = multiply_matrix(T, Dx_l1);
+	T = multiply_matrix(T, Ry_t2);
+	T = multiply_matrix(T, Dy_d2);
+	T = multiply_matrix(T, Dx_l2);
+	T = multiply_matrix(T, Ry_t3);
+	T = multiply_matrix(T, Dy_d3);
+	T = multiply_matrix(T, Dz_d4);
+	T = multiply_matrix(T, Dx_l3);
+
+	x[0] = T[0][3];
+	x[1] = T[1][3];
+	x[2] = T[2][3];
 }
 
-int rotation_y(double theta,double R[][4])
+
+// inverse kinematics function
+void inv_kin(double x[3], double theta[6])
 {
-	double Ry[4][4] = {{cos(theta),0,sin(theta),0},{0,1,0,0},{-1*sin(theta),0,cos(theta),0},{0,0,0,1}};
-	int i,j;
-	for(i=0;i<4;i++)
-	{
-		for(j=0;j<4;j++)
-		{
-			R[i][j] = Ry[i][j];
-		}
-	}
-	return 0;
+	// code
 }
 
-int rotation_z(double theta,double R[][4])
-{
-	double Rz[4][4] = {{cos(theta),-1*sin(theta),0,0},{sin(theta),cos(theta),0,0},{0,0,1,0},{0,0,0,1}};
-	int i,j;
-	for(i=0;i<4;i++)
-	{
-		for(j=0;j<4;j++)
-		{
-			R[i][j] = Rz[i][j];
-		}
-	}
-	return 0;
-}
-
-
-fwd_kin(theta, x)
-double *theta;
-double x[3];
-{
-
-	double length[] = {0.25,0.25,0.25,0.15};
-
-	double Dzlo[4][4]; 
-	translation(0,0,length[0],Dzlo);
-	double Dxl1[4][4];
-	translation(length[1],0,0,Dxl1);
-	double Dxl2[4][4];
-	translation(length[2],0,0,Dxl2);
-	double Dxl3[4][4];
-	translation(length[3],0,0,Dxl3);
-
-
-	double d1 = 0.05;
-	double d2 = 0.05;
-	double Dyd1[4][4];
-	double Dzd2[4][4];
-	translation(0,d1,0,Dyd1);
-	translation(0,0,d2,Dzd2);
-
-	double Rzt0[4][4];
-	double Ryt1[4][4];
-	double Ryt2[4][4];
-	double Ryt3[4][4];
-	double Rxt4[4][4];
-	rotation_z(theta[0],Rzt0);
-	rotation_y(theta[1],Ryt1);
-	rotation_y(theta[2],Ryt2);
-	rotation_y(theta[3],Ryt3);
-	rotation_y(theta[4],Rxt4);
-
-	double _3T4_1[4][4],_3T4_2[4][4],_3T4[4][4];
-	matrix_multiplication(Dxl3,Rxt4,_3T4_1);
-	matrix_multiplication(Dzd2,_3T4_1,_3T4_2);
-	matrix_multiplication(Dyd1,_3T4_2,_3T4);
-
-	double _2T3_1[4][4],_2T3[4][4];
-	matrix_multiplication(Dxl2,Ryt3,_2T3_1);
-	matrix_multiplication(Dyd1,_2T3_1,_2T3);
-
-	double _1T2[4][4],_bT0[4][4];
-	matrix_multiplication(Dxl1,Ryt2,_1T2);
-
-	matrix_multiplication(Dzlo,Rzt0,_bT0);
-
-	double _R1[4][4],_R2[4][4],_R3[4][4],_R[4][4];
-	matrix_multiplication(_2T3,_3T4,_R1);
-	matrix_multiplication(_1T2,_R1,_R2);
-	matrix_multiplication(Ryt1,_R2,_R3);
-	matrix_multiplication(_bT0,_R3,_R);
-
-	x[0] = _R[0][3];
-	x[1] = _R[1][3];
-	x[2] = _R[2][3];
-
-
-}
-
-
-inv_kin(x, theta)
-double *x;
-double theta[6];
-{
-
-	double _X = x[0];
-	double _Y = x[1];
-	double _Z = x[2];
-
-	double l0 = 0.25;
-	double l1 = 0.25;
-	double l2 = 0.25;
-	double l3 = 0.15;
-
-	double d1 = 0.05;
-	double d2 = 0.05;
-
-	double powX = pow(_X,2);
-	double powY = pow(_Y,2);
-	double powD1 = pow(d1,2);
-
-	double a = atan(_X/_Y);
-	double R = sqrt(pow(_X,2)+pow(_Y,2));
-	double theta0 = acos((2*d1)/R) - a;
-
-	if(_X<0 && _Y<0)
-	{
-		theta0 = theta0 - M_PI;
-	}
-	else if(_Y < 0)
-	{
-		theta0 = theta0 - M_PI;
-	}
-
-	double powl1 = pow(l1,2);
-	double powl2 = pow(l2,2);
-
-	double X1 = (_X*cos(theta0)+_Y*sin(theta0))-d2;
-	double Z1 = _Z-l0+l3;
-	double r = pow(X1,2)+pow(Z1,2);
-	double alpha = atan(Z1/X1);
-	double beta = acos((powl1-powl2+r)/(2*l1*sqrt(r)));
-	double theta1 = -1 * (beta + alpha);
-
-
-	double gamma = acos((powl1+powl2-r)/(2*l1*l2));
-	double theta2 = M_PI - gamma;
-
-	double phy2 = M_PI/2 + alpha; 
-	double phy1 = acos((powl2-powl1+r)/(2 * l2 * sqrt(r)));
-
-	double theta3 = ( phy2 - phy1);
-
-	double thetaD = 0.0;
-	theta[0] = theta0;
-	theta[1] = theta1;
-	theta[2] = theta2;
-	theta[3] = theta3;
-	theta[4] = thetaD;
-	theta[5] = thetaD;
-
-}
